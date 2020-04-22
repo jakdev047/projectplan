5251bbf72db7590eef1cc15d5e36c7d35887cbfe
jakdev047 <shuvo047niter@gmail.com>
2020-04-22 14:30:46 +0600

    to add react-router-dom, Nav & Dashboard Component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import './assets/sass/index.scss';
 
 // components
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import Error from './components/Error/Error';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar/>
+        <Switch>
+          <Route exact path='/' component={Dashboard}/>
+          <Route path='*' component={Error}/>
+        </Switch>
       </div>
     </Router>
   );
