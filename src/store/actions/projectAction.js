f094894273e04fffad93e4512f8aa6499ca406f8
jakdev047 <shuvo047niter@gmail.com>
2020-04-22 19:13:04 +0600

    to add redux-firestore & react-redux-firebase

--- src/store/actions/projectAction.js
+++ src/store/actions/projectAction.js
@@ -1,6 +1,6 @@
 import { ADD_PROJECT } from "./types";
 
-export const createProject = project => dispatch => {
+export const createProject = project => (dispatch,{getFirebase,getFirestore}) => {
   dispatch({
     type: ADD_PROJECT,
     payload: project
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,24 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
-const middleware = [thunk];
+import { getFirestore, reduxFirestore } from "redux-firestore";
+import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
+
+import firebaseConfig from '../config/firebaseConfig';
+
+const middleware = [thunk.withExtraArgument({
+  getFirebase,
+  getFirestore
+})];
 
 const store = createStore(
   rootReducer,
   {},
-  composeWithDevTools(applyMiddleware(...middleware))
+  composeWithDevTools(
+    applyMiddleware(...middleware),
+    reduxFirestore(firebaseConfig),
+    reactReduxFirebase(firebaseConfig)
+  )
 )
 
 export default store;
